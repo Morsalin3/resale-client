dc56ce8a2e70677257069e658bbccd7ab10f76e3
Morsalin3 <mdmorsalin3@gmail.com>
2022-11-27 02:23:31 +0600

    added dashboard

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -6,7 +6,22 @@ const DashboardLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <Outlet></Outlet>
+            <div className="drawer drawer-mobile">
+                <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content">
+
+                    <Outlet></Outlet>
+
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+                    </ul>
+
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -25,7 +25,7 @@ const Navbar = () => {
         }
     </React.Fragment>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 flex justify-between">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -37,11 +37,15 @@ const Navbar = () => {
                 </div>
                 <Link to='/' className="btn btn-ghost normal-case text-xl">Swap</Link>
             </div>
+
             <div className="navbar-end mx-10 hidden lg:flex">
                 <ul className="menu menu-horizontal p-0 ">
                     {menuItems}
                 </ul>
             </div>
+            <label htmlFor="dashboard-drawer" tabIndex={2} className="btn  btn-ghost lg:hidden ">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
         </div>
     );
 };
